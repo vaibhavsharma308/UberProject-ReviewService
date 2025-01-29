a5c35ea4be920a73821b66a8b128561321e29ab3
vaibhavsharma308 <vs128646@gmail.com>
2025-01-29 19:21:48 +0530

    Add some columns to our review Model .

--- src/main/java/com/vaibhavCodes/uberReviewService/models/Review.java
+++ src/main/java/com/vaibhavCodes/uberReviewService/models/Review.java
@@ -1,11 +1,33 @@
 package com.vaibhavCodes.uberReviewService.models;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 
 @Entity
+@Table(name="booking_review")
 public class Review {
     @Id             // This makes the Id Property as the Primary Key of our table.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  //Auto Increment
     Long id;
 
+    @Column(nullable = false)
+    String content;
+
+    double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    @Column(nullable = false)
+    Date updatedAt;
 }
+
+//Allow Spring to do this
+// new Review(content,rating)
